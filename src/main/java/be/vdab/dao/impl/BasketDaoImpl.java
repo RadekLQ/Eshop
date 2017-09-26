450befe329654a95039793786a1d6613e8950224
Radek <radoslaw.lizak@gmail.com>
2017-09-26 14:05:02 +0200

    BasketDaoImpl temp2

--- src/main/java/be/vdab/dao/impl/BasketDaoImpl.java
+++ src/main/java/be/vdab/dao/impl/BasketDaoImpl.java
@@ -4,12 +4,12 @@ import be.vdab.dao.BasketDao;
 import be.vdab.entiteiten.Basket;
 import be.vdab.entiteiten.Product;
 import com.google.common.collect.Lists;
+import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import org.apache.log4j.Logger;
 import java.util.Properties;
 
 
@@ -19,7 +19,7 @@ public class BasketDaoImpl implements BasketDao {
     private static final Logger LOGGER = Logger.getLogger(BasketDaoImpl.class);
 
     private static final String KOLOM_IDPRODUCT = "productId";
-    private static final String KOLOM_NAME= "name";
+    private static final String KOLOM_NAME = "name";
     private static final String KOLOM_PRICE = "price";
     private static final String KOLOM_STOCK = "stock";
 
@@ -29,22 +29,27 @@ public class BasketDaoImpl implements BasketDao {
         producten = Lists.newArrayList();
     }
 
+
+
+    @Override
+    public void saveOrUpdateBasket(Basket basket) {
+        producten.addAll(producten);
+    }
+
     @Override
     public void addProductToBasket(Product product) {
         String sql = "INSERT INTO product (productId, name, price, stock) VALUES(?,?,?,?);";
 
         try (Connection con = getConnection(); PreparedStatement stmt = con.prepareStatement(sql)) {
 
-            stmt.setInt(1, product.getProductNummer());
-            stmt.setString(2, product.getMerk());
-            stmt.setString(3, product.getNaam());
-            stmt.setDouble(4, product.getPrijs());
-            stmt.setInt(5, product.getVolume());
-            stmt.setInt(6, getTypeId(product));
+            stmt.setInt(1, product.getProductId());
+            stmt.setString(2, product.getProductname());
+            stmt.setDouble(3, product.getPrice());
+            stmt.setInt(4, product.getStock());
 
             int result = stmt.executeUpdate();
 
-            LOGGER.debug(result + " toegevoegd aan de tabel product");
+            LOGGER.debug(result + " product added to basket");
 
         } catch (SQLException e) {
             LOGGER.error("Could nog connect to database: " + e);
@@ -52,26 +57,39 @@ public class BasketDaoImpl implements BasketDao {
     }
 
     @Override
-    public void saveOrUpdateBasket(Basket basket) {
-        producten.addAll(producten);
-    }
-
-
-
-    @Override
     public void removeProductFromBasket(Product product) {
-        producten.add(product);
+        String sql = "DELETE product (productId, name, price, stock) VALUES(?,?,?,?);";
+
+        try (Connection con = getConnection(); PreparedStatement stmt = con.prepareStatement(sql)) {
+
+            stmt.setInt(1, product.getProductId());
+            stmt.setString(2, product.getProductname());
+            stmt.setDouble(3, product.getPrice());
+            stmt.setInt(4, product.getStock());
+
+            int result = stmt.executeUpdate();
+
+            LOGGER.debug(result + " product added to basket");
+
+        } catch (SQLException e) {
+            LOGGER.error("Could nog connect to database: " + e);
+        }
     }
 
     @Override
     public Basket getBasket() {
+        addToList(SELECT_SQL);
         return null;
+    }
 
+    public List<Product> listOfAllItemsInBasket() {
+        addToList(SELECT_SQL);
+        return producten;
     }
 
     @Override
     public void clearBasket() {
-        producten.clear();
+        addToList(SELECT_SQL + "");
     }
 
     private Connection getConnection() throws SQLException {
@@ -84,4 +102,29 @@ public class BasketDaoImpl implements BasketDao {
         }
         return DriverManager.getConnection(prop.getProperty("jdbc.url"), prop.getProperty("jdbc.user"), prop.getProperty("jdbc.password"));
     }
+
+    private void addToList(String sql) {
+
+        producten = new ArrayList<>();
+
+        try (Connection con = getConnection(); Statement stmt = con.createStatement()) {
+
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+
+                int ProductId = rs.getInt(KOLOM_IDPRODUCT);
+                String name = rs.getString(KOLOM_NAME);
+                double price = rs.getDouble(KOLOM_PRICE);
+                int stock = rs.getInt(KOLOM_STOCK);
+
+                producten.add(new Product(ProductId, name, price, stock));
+            }
+
+        } catch (SQLException e) {
+            LOGGER.error("Could nog connect to database: " + e);
+        }
+    }
+
+
 }
