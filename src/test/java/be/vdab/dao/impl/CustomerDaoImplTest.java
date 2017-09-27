fc69a062d3c69996656678d780e311ebe847c9ef
Radek <radoslaw.lizak@gmail.com>
2017-09-27 14:51:54 +0200

    CustomerDaoImplTest testing with Junit5

--- src/test/java/be/vdab/dao/impl/CustomerDaoImplTest.java
+++ src/test/java/be/vdab/dao/impl/CustomerDaoImplTest.java
@@ -1,18 +1,29 @@
 package be.vdab.dao.impl;
 
+import be.vdab.entiteiten.Customer;
+import be.vdab.entiteiten.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+//https://github.com/howtoprogram/junit5-examples/blob/master/junit5-maven-example/src/test/java/com/howtoprogram/junit5/BasicSalaryCalculatorTest.java
 class CustomerDaoImplTest {
+    private Customer customer;
+    private User user;
+
     @BeforeEach
-    void setUp() {
+    void init() {
+        customer = new Customer("user123", "password123",
+                '1', "Armani", "Giorgio ", "jan.armani@armani.com",
+                "Via Alessandro Manzoni 31, 20121 Milano MI, Italy");
+        user = new User();
     }
 
     @AfterEach
     void tearDown() {
+        customer = null;
+        user = null;
     }
 
     @Test
@@ -21,6 +32,14 @@ class CustomerDaoImplTest {
 
     @Test
     void findByLoginAndUsername() {
+        String username = "user123";
+        user.setUsername(username);
+
+        String password = "password123";
+        user.setPassword(password);
+
+
+
     }
 
     @Test
