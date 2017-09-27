f2eefd46e89a4e0af502d8c630cbf4c1278cfcb9
Radek <radoslaw.lizak@gmail.com>
2017-09-27 15:24:28 +0200

    CustomerDaoImplTest testing with Junit5

--- src/test/java/be/vdab/dao/impl/CustomerDaoImplTest.java
+++ src/test/java/be/vdab/dao/impl/CustomerDaoImplTest.java
@@ -4,7 +4,11 @@ import be.vdab.entiteiten.Customer;
 import be.vdab.entiteiten.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 //https://github.com/howtoprogram/junit5-examples/blob/master/junit5-maven-example/src/test/java/com/howtoprogram/junit5/BasicSalaryCalculatorTest.java
@@ -31,15 +35,38 @@ class CustomerDaoImplTest {
     }
 
     @Test
-    void findByLoginAndUsername() {
+    void testFindByLoginAndUsernameWithValidInput() {
         String username = "user123";
         user.setUsername(username);
 
         String password = "password123";
         user.setPassword(password);
 
+        User expectedUser = new User("user123", "password123");
+        assertEquals(expectedUser, user);
+    }
 
+    @DisplayName("Test findByLoginAndUsername with invalid input")
+    @Test
+    void testFindByLoginAndUsernameWithInvalidUsername() {
 
+        String username = "";
+        String password = "password123";
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            user.setUsername(username);
+        });
+    }
+
+    @DisplayName("Test findByLoginAndUsername with invalid input")
+    @Test
+    void testFindByLoginAndUsernameWithInvalidPassword() {
+
+        String password = "";
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            user.setPassword(password);
+        });
     }
 
     @Test
