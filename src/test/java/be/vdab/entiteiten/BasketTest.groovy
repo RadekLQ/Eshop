4d807657efa681b8d893b36ed7e6af56083b20ca
Radek <radoslaw.lizak@gmail.com>
2017-09-27 12:32:23 +0200

    testing Basket

---
+++ src/test/java/be/vdab/entiteiten/BasketTest.groovy
@@ -0,0 +1,13 @@
+package be.vdab.entiteiten
+
+class BasketTest extends GroovyTestCase {
+
+    void testAddToBasket() {
+    }
+
+    void testRemoveFromBasket() {
+    }
+
+    void testGetProducten() {
+    }
+}
